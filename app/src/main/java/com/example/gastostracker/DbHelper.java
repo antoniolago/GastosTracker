3cfb5b84d2a990cfb5200c415101466f406204cd
antoniolago <antonio.clago@outlook.com>
2019-09-22 20:51:20 -0300

    0.00002v

--- app/src/main/java/com/example/gastostracker/DbHelper.java
+++ app/src/main/java/com/example/gastostracker/DbHelper.java
@@ -9,35 +9,12 @@ import androidx.annotation.Nullable;
 public class DbHelper extends SQLiteOpenHelper {
     private static final String NOME_BANCO = "banco.db";
     private static final int VERSAO = 1;
-    private final String CRIA_CLIENTE = "CREATE TABLE tbCliente (" +
-            "pkCliente INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            "Nome TEXT NOT NULL, Sexo TEXT, UF TEXT NOT NULL)";
-
     private final String CRIA_GASTOS = "CREATE TABLE tbGastos (" +
             "pkGasto INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            "Descricao TEXT NOT NULL, " +
-            "Valor TEXT, " +
-            "fkCliente INTEGER NOT NULL, " +
-            "fkCategoria INTEGER NOT NULL," +
-            "FOREIGN KEY(fkCategoria) REFERENCES tbCategorias(pkCategoria)" +
-            "FOREIGN KEY(fkCliente) REFERENCES tbClientes(pkCliente))";
+            "descricao TEXT NOT NULL, " +
+            "valor TEXT, " +
+            "tipoGasto INTEGER);";
 
-    private final String CRIA_CATEGORIAS = "CREATE TABLE tbCategorias (" +
-            "pkCategoria INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            "Nome TEXT NOT NULL, Descricao TEXT, UF TEXT NOT NULL, ";
-    private final String POPULA_CATEGORIAS = "" +
-            "INSERT INTO tbCategorias (Nome)\n" +
-            "VALUES ('Saude'); " +
-            "INSERT INTO tbCategorias (Nome)\n" +
-            "VALUES ('Gerais'); " +
-            "INSERT INTO tbCategorias (Nome)\n" +
-            "VALUES ('Alimentacao'); " +
-            "INSERT INTO tbCategorias (Nome)\n" +
-            "VALUES ('Transporte'); " +
-            "INSERT INTO tbCategorias (Nome)\n" +
-            "VALUES ('Doacoes'); " +
-            "INSERT INTO tbCategorias (Nome)\n" +
-            "VALUES ('Contas'); ";
 
     public DbHelper(Context context) {
         super(context, NOME_BANCO, null, VERSAO);
@@ -45,10 +22,7 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CRIA_CATEGORIAS);
-        db.execSQL(CRIA_CLIENTE);
         db.execSQL(CRIA_GASTOS);
-        db.execSQL(POPULA_CATEGORIAS);
     }
 
     @Override
