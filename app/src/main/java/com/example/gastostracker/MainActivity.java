a5c922f67a082d723030364f8ff7c735b271a226
antonioclago <antonio.lago.eu@hotmail.com>
2019-09-22 22:24:55 -0300

    0.00002v

--- app/src/main/java/com/example/gastostracker/MainActivity.java
+++ app/src/main/java/com/example/gastostracker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.gastostracker;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnAddGasto = findViewById(R.id.btnAddGasto);
+        btnAddGasto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this,CadastroActivity.class);
+                startActivity(it);
+            }
+        });
+        Button btnListaGasto = findViewById(R.id.btnListaGasto);
+        btnListaGasto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this,CadastroActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
